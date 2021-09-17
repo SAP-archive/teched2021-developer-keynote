294a92d9ba002603d90c59cba031b0f6e47f2410
Thomas Jung <thomas.jung@sap.com>
2021-09-17 12:39:03 -0400

    Add CAP OData V2 Proxy

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const cds = require('@sap/cds')
 const cors = require('cors')
+const proxy = require('@sap/cds-odata-v2-adapter-proxy')
 
 cds.on('bootstrap', app => {
+    app.use(proxy())
     app.use(cors()) 
     app.get('/health', (_, res) => {
         res.status(200).send('OK')
