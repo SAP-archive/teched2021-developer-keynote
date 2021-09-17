c93afbfe22454725e25ba17ae73648cab5218747
Thomas Jung <thomas.jung@sap.com>
2021-09-17 11:55:47 -0400

    Add CORS and SwaggerUI to CAP service

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
 const cds = require('@sap/cds')
+const cors = require('cors')
 
 cds.on('bootstrap', app => {
+    app.use(cors()) 
     app.get('/health', (_, res) => {
         res.status(200).send('OK')
     })
 })
+
+if (process.env.NODE_ENV !== 'production') {
+    const cds_swagger = require('cds-swagger-ui-express')
+    cds.on('bootstrap', app => app.use(cds_swagger()))
+}
 
 module.exports = cds.server
